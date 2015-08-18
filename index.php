ef86b6ea208e09eccdf0963857159fb92714376d
Kyle Lehtinen <kylelehtinen@gmail.com>
2015-08-18 10:58:55 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Home View</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<?php 
+		echo "hello";
+	?>
+
+</body>
+<footer>
+	&copy; 2015
+</footer>
+</html>
