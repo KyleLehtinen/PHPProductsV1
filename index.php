47e0c20f393f1958ccb0eac31ed6ba65eb348b6a
Kyle Lehtinen <kylelehtinen@gmail.com>
2015-08-18 14:06:07 -0700

    Tweaked CSS finished index.html

--- index.php
+++ index.php
@@ -7,11 +7,21 @@
 </head>
 <body>
 
-	<?php 
-	include('header.php');
-	include('footer.php');
+	<?php require('header.php'); ?>
+	
+	<h1>Welcome to our shopping cart</h1>
+	<p>Do you want to see our list of <a href="browse.php">products</a></p>
 
-	?>
+	<?php
+	/*echo "<h1>Welcome to our shopping cart</h1>";
+	$name = "Kyle";
+	$age = 26;
+	$message = "Hello $name you are $age";
+	echo($message);
+	echo "<br>Hello '".$name."' you are ".$age;*/
+
+	require('footer.php');?>
+	
 
 </body>
 
